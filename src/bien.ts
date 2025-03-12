68b6110be43d532871bd2a29be73906e53c591b2
Iván Pérez <alu0101477510@ull.edu.es>
2025-03-12 20:08:58 +0000

    clases

---
+++ src/bien.ts
@@ -0,0 +1,21 @@
+/**
+ * Representa un bien (objeto) de la Posada del Lobo Blanco
+ */
+export class Bien {
+  /**
+   * @param id - Identificador único del bien.
+   * @param nombre - Nombre del bien.
+   * @param descripcion - Descripción que detalla el origen y utilidad.
+   * @param material - Material principal del bien.
+   * @param peso - Peso del bien.
+   * @param valor - Valor en coronas.
+   */
+  constructor(
+    public id: string,
+    public name: string,
+    public description: string,
+    public material: string,
+    public weight: number,
+    public price: number
+  ) {}
+}
---
+++ src/mercader.ts
@@ -0,0 +1,8 @@
+export class Mercader {
+  constructor(
+    public id: number,
+    public nombre: string,
+    public tipo: string,
+    public ubicacion: string
+  ) {}
+}
