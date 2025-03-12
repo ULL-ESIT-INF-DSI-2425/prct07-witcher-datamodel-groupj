609f244738a278b2c401b1d9d66482206c64342a
Oscar Navarro <alu0101504094@ull.edu.es>
2025-03-12 19:18:32 +0000

    Instalación de Inquirer y LowDB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-/**
- * 
- * @param saludo - saludo
- * @returns - saludo
- */
-export function Saludar(saludo: string): string {
-  return saludo;
+import { JSONFilePreset } from 'lowdb/node';
+
+async function main() {
+  // Read or create db.json
+  const defaultData: { posts: string[] } = { posts: [] };
+  const db = await JSONFilePreset('db.json', defaultData);
+
+  // Update db.json
+  await db.update(({ posts }) => posts.push('hello world'));
+
+  // Alternatively you can call db.write() explicitly later
+  db.data.posts.push('hello world');
+  await db.write();
 }
+
+main().catch(console.error);
