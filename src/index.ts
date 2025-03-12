57afcade13e8d46d12a6e9dbcd0caaa34a962c44
mag4no10 <alu0101470948@ull.edu.es>
2025-03-12 19:58:10 +0000

    test: testing branches

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ async function main() {
   // Crear o leer la base de datos con esquema
   const defaultData: DBSchema = { posts: [] };
   const db = await JSONFilePreset<DBSchema>('db.json', defaultData);
+  console.log("oscar chupapollas");
 
   // Agregar un nuevo post
   db.data.posts.push('hello world');
